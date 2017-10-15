b01a97cce300516fc5dedbc9a94fe4984cd82d11
James Benoit <jrabenoit@gmail.com>
2017-10-14 19:02:36 -0600

    bootstrap working

--- bootstrap.py
+++ bootstrap.py
@@ -2,21 +2,27 @@
 
 import numpy as np
 from collections import defaultdict
-import pprint, itertools
-import random
+import pprint, itertools, pickle, random
 
-def EmpiricalDistro():
-    sample_size = int(input('\nSample size: '))*2
-    sample_distro = []
-    n_iterations = 10000
-    for i in range(n_iterations):
-        sample_list = []
-        #Because we are sampling with replacement, we don't need to worry about the program picking all subjects each time... some may be picked more than once, and the total number of samples will be equal to the number of subjects.
-        sample_list = np.random.choice(list(per_subject_test_acc.values()), sample_size,replace=True) 
-        sample_mean = sum(sample_list)/len(sample_list)
-        sample_distro.append(sample_mean)
+def Bill():
+    with open('/media/james/ext4data1/current/projects/ramasubbu/inner_test_results.pickle','rb') as f: inner_test=pickle.load(f)
     
-    p_value = sum(i <= 50.0 for i in sample_distro)/n_iterations
-    print(p_value)
+    g=inner_test.groupby('subjects')    
+    
+    #Per subject accuracy
+    acc=(g['scores'].sum()/g['attempts'].sum())*100
+    n = len(acc)
+    runs = 10000
+
+#Because we are sampling with replacement, we don't need to worry about the program picking all subjects each time... some may be picked more than once, and the total number of samples will be equal to the number of subjects.
+    distribution = []
+    for i in range(runs):        
+        sample_list = np.random.choice(acc, n,replace=True) 
+        sample_mean = sum(sample_list)/len(sample_list)
+        distribution.append(sample_mean)
+    
+    print('{} runs of {} samples'.format(len(distribution), n))
+    print('distribution mean: {}%'.format(sum(distribution)/len(distribution)))
+    print('p-value: {}'.format(sum(i <= 50 for i in distribution)/runs))
     
     return
--- estimators.py
+++ estimators.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import pandas as pd
 import copy, pickle
 from sklearn import svm, naive_bayes, neighbors, ensemble, linear_model, tree, discriminant_analysis
 
@@ -18,92 +19,118 @@ def InnerFolds():
           'lda': discriminant_analysis.LinearDiscriminantAnalysis()
           }
    
-    train_results={'fold':[], 'estimator':[], 'subjects':[], 'labels':[], 'predictions':[], 'scores':[]}
-    test_results={}
+    train_results={'fold':[], 'estimator':[], 'subjects':[], 'labels':[], 'predictions':[], 'scores':[], 'attempts':[]}
+    test_results={'fold':[], 'estimator':[], 'subjects':[], 'labels':[], 'predictions':[], 'scores':[], 'attempts':[]}
    
     for i in range(folds):
         with open('/media/james/ext4data1/current/projects/ramasubbu/innercvfeatures/fold_'+str(i)+'_train.pickle','rb') as f: X_train=pickle.load(f)
         with open('/media/james/ext4data1/current/projects/ramasubbu/innercvfeatures/fold_'+str(i)+'_test.pickle','rb') as f: X_test=pickle.load(f)
-                
-        fold=[str(i+1)]*len(cv['X_train'][i])
-        subjects= cv['X_train'][i]
-        labels= cv['y_train'][i]
+        y_train= cv['y_train'][i]
+        y_test= cv['y_test'][i]        
         
         print('\nFold {}/{}\n'.format(i+1, folds))   
-        
-        y_train= cv['y_train'][i]
-        y_test= cv['y_test'][i]
-        
+
         for j,k in zip(est.keys(), est.values()):
             k.fit(X_train, y_train)
-            estimator=[j]*len(subjects)
+            fold=[str(i+1)]*len(cv['X_train'][i])  
+            estimator=[j]*len(cv['X_train'][i])
+            subjects= cv['X_train'][i]
+            labels= cv['y_train'][i]
             predictions= k.predict(X_train)
             scores= [1 if x==y else 0 for x,y in zip(labels, predictions)]
-            print(scores)
+            attempts=[1]*len(cv['X_train'][i])
             train_results['fold'].extend(fold)
             train_results['estimator'].extend(estimator)
             train_results['subjects'].extend(subjects)
             train_results['labels'].extend(labels)
             train_results['predictions'].extend(predictions)
             train_results['scores'].extend(scores)
-                          
+            train_results['attempts'].extend(attempts)
         
-    #print('Test accuracies:\n{}'.format(np.add.reduceat(test_results['test'], np.arange(0, folds, 2))*100/2))    
+        for j,k in zip(est.keys(), est.values()):    
+            k.fit(X_test, y_test)
+            fold=[str(i+1)]*len(cv['X_test'][i])  
+            estimator=[j]*len(cv['X_test'][i])
+            subjects= cv['X_test'][i]
+            labels= cv['y_test'][i]
+            predictions= k.predict(X_test)
+            scores= [1 if x==y else 0 for x,y in zip(labels, predictions)]
+            attempts=[1]*len(cv['X_test'][i])
+            test_results['fold'].extend(fold)
+            test_results['estimator'].extend(estimator)
+            test_results['subjects'].extend(subjects)
+            test_results['labels'].extend(labels)
+            test_results['predictions'].extend(predictions)
+            test_results['scores'].extend(scores)                   
+            test_results['attempts'].extend(attempts)
 
-    with open('/media/james/ext4data1/current/projects/ramasubbu/train_results.pickle', 'wb') as d: pickle.dump(train_results, d, pickle.HIGHEST_PROTOCOL) 
-    with open('/media/james/ext4data1/current/projects/ramasubbu/test_results.pickle', 'wb') as d: pickle.dump(test_results, d, pickle.HIGHEST_PROTOCOL) 
+    train_results=pd.DataFrame(train_results)
+    test_results=pd.DataFrame(test_results)
+    
+    with open('/media/james/ext4data1/current/projects/ramasubbu/inner_train_results.pickle', 'wb') as d: pickle.dump(train_results, d, pickle.HIGHEST_PROTOCOL) 
+    with open('/media/james/ext4data1/current/projects/ramasubbu/inner_test_results.pickle', 'wb') as d: pickle.dump(test_results, d, pickle.HIGHEST_PROTOCOL) 
 
     return
-    
-def OuterFolds():
-    with open('/media/james/ext4data1/current/projects/ramasubbu/outer_cv.pickle','rb') as f: cv=pickle.load(f)       
-    
-    scores={'fold': [], 'train':[], 'test':[]}
-    
-    for i in range(5):
-        with open('/media/james/ext4data1/current/projects/ramasubbu/outercvfeatures/fold_'+str(i)+'_train.pickle','rb') as f:    
-            X_train=pickle.load(f)
-        with open('/media/james/ext4data1/current/projects/ramasubbu/outercvfeatures/fold_'+str(i)+'_test.pickle','rb') as f:
-            X_test=pickle.load(f)
-        
-        y_train= cv['y_train'][i]
-        y_test= cv['y_test'][i]
-        
-        #est= ensemble.RandomForestClassifier()
-        #est= ensemble.ExtraTreesClassifier()
-        #est= neighbors.KNeighborsClassifier()
-        #est= naive_bayes.GaussianNB()
-        #est= tree.DecisionTreeClassifier()
-        #est= svm.LinearSVC()
-        est= discriminant_analysis.LinearDiscriminantAnalysis()
-        
-        est.fit(X_train, y_train)
-                
-        print('\nFold {}/5\n'.format((i+1)))
-        
-        predicted_train= est.predict(X_train)
-        train_score= est.score(X_train, y_train)
-        print('X_train predictions: {}'.format(predicted_train))
-        print('y_train actual vals: {}'.format(y_train))        
-        print('Training set score: {}%\n'.format((train_score*100)))
-        
-        predicted_test= est.predict(X_test)
-        test_score= est.score(X_test, y_test)
-        print('X_test predictions: {}'.format(predicted_test))
-        print('y_test actual vals: {}'.format(y_test))
-        print('Baseline accuracy: {}'.format('50%'))
-        print('Test set score: {}%\n'.format((test_score*100)))
 
-        scores['fold'].append(i)
-        scores['train'].append(train_score)
-        scores['test'].append(test_score)
-                
-    print('All Train Average Acc: {}%'.format((sum(scores['train'])/5)*100))
-    print('All Test Average Acc: {}%'.format((sum(scores['test'])/5)*100))
-    #We set this to 50% because of the stratified k-fold CV
-    print('All Test Expected Acc: {}%'.format('50%'))
-    print('Combined acc per outer fold:\n{}'.format(np.add.reduceat(scores['test'], np.arange(0, 5, 1))*100))    
+def OuterFolds():
+    with open('/media/james/ext4data1/current/projects/ramasubbu/outer_cv.pickle','rb') as f: cv=pickle.load(f)
+
+    folds=5
+    
+    est= {#'randomforest': ensemble.RandomForestClassifier(), 
+          #'extratrees': ensemble.ExtraTreesClassifier(),
+          #'kneighbors': neighbors.KNeighborsClassifier(),
+          'naivebayes': naive_bayes.GaussianNB(),
+          #'decisiontree': tree.DecisionTreeClassifier(),
+          #'linearsvm': svm.LinearSVC(),
+          #'lda': discriminant_analysis.LinearDiscriminantAnalysis()
+          }
+   
+    train_results={'fold':[], 'estimator':[], 'subjects':[], 'labels':[], 'predictions':[], 'scores':[]}
+    test_results={'fold':[], 'estimator':[], 'subjects':[], 'labels':[], 'predictions':[], 'scores':[]}
+   
+    for i in range(folds):
+        with open('/media/james/ext4data1/current/projects/ramasubbu/outercvfeatures/fold_'+str(i)+'_train.pickle','rb') as f: X_train=pickle.load(f)
+        with open('/media/james/ext4data1/current/projects/ramasubbu/outercvfeatures/fold_'+str(i)+'_test.pickle','rb') as f: X_test=pickle.load(f)
+        y_train= cv['y_train'][i]
+        y_test= cv['y_test'][i]        
         
-    with open('/media/james/ext4data1/current/projects/ramasubbu/est_scores.pickle', 'wb') as d: pickle.dump(scores, d, pickle.HIGHEST_PROTOCOL) 
+        print('\nFold {}/{}\n'.format(i+1, folds))   
+
+        for j,k in zip(est.keys(), est.values()):
+            k.fit(X_train, y_train)
+            fold=[str(i+1)]*len(cv['X_train'][i])  
+            estimator=[j]*len(cv['X_train'][i])
+            subjects= cv['X_train'][i]
+            labels= cv['y_train'][i]
+            predictions= k.predict(X_train)
+            scores= [1 if x==y else 0 for x,y in zip(labels, predictions)]
+            train_results['fold'].extend(fold)
+            train_results['estimator'].extend(estimator)
+            train_results['subjects'].extend(subjects)
+            train_results['labels'].extend(labels)
+            train_results['predictions'].extend(predictions)
+            train_results['scores'].extend(scores)
+        
+        for j,k in zip(est.keys(), est.values()):    
+            k.fit(X_test, y_test)
+            fold=[str(i+1)]*len(cv['X_test'][i])  
+            estimator=[j]*len(cv['X_test'][i])
+            subjects= cv['X_test'][i]
+            labels= cv['y_test'][i]
+            predictions= k.predict(X_test)
+            scores= [1 if x==y else 0 for x,y in zip(labels, predictions)]
+            test_results['fold'].extend(fold)
+            test_results['estimator'].extend(estimator)
+            test_results['subjects'].extend(subjects)
+            test_results['labels'].extend(labels)
+            test_results['predictions'].extend(predictions)
+            test_results['scores'].extend(scores)                   
+    
+    train_results=pd.DataFrame(train_results)
+    test_results=pd.DataFrame(test_results)
+    
+    with open('/media/james/ext4data1/current/projects/ramasubbu/outer_train_results.pickle', 'wb') as d: pickle.dump(train_results, d, pickle.HIGHEST_PROTOCOL) 
+    with open('/media/james/ext4data1/current/projects/ramasubbu/outer_test_results.pickle', 'wb') as d: pickle.dump(test_results, d, pickle.HIGHEST_PROTOCOL) 
 
     return
