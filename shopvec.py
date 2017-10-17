e6a5837469ae1418e64102f5cb827e58ac7642c3
James Benoit <jrabenoit@gmail.com>
2017-10-17 12:44:27 -0600

    corrected mistake in bootstrap

--- shopvec.py
+++ shopvec.py
@@ -4,36 +4,32 @@ import crossval, features, estimators, estpicker, bootstrap
 
 def Vecuum():
     
-    print('GROUP CHOICES')
+    print('\nGROUP CHOICES (automated order: 2,1,0,4,3,5,11,10,12)')
     print('\nSymptom Severity:\n')
     print(' 0= control/mild\n 1= control/severe\n 2= control/very severe\n 3= mild/severe\n 4= mild/very severe\n 5= severe/very severe\n 6= control/all patients\n')
     
     print('Treatment Response:\n')
-    print(' 7= control/non-responder\n 8= control/all responder\n 9= control/remitter only\n 10= non-responder/all responder\n 11= non-responder/remitter only\n 12= responder vs remitter\n 13= control/all patients')
+    print(' 7= control/non-responder\n 8= control/all responder\n 9= control/remitter only\n 10= non-responder/all responder\n 11= non-responder/remitter only\n 12= responder vs remitter\n 13= control/all patients\n')
 
-    group= int(input('\nChoice: '))
+    groups= [2,1,0,
+            4,3,5,
+            11,10,12]
     runs=10
 
-    run=1
-    for i in range(runs):    
-        print('BEGINNING RUN {}/{}'.format(run, runs))
-
-        crossval.OuterCV(group)
-        crossval.InnerCV()
-        
-        features.SelKBest()
-        features.SelKBestOuter()
-
-        estimators.InnerFolds(group, run)
-        bestest= estpicker.Best(group, run)
-        
-        estimators.OuterFolds(group, run, bestest)
-        
-        print('BOOTSTRAP')
-        bootstrap.Bill(group, run)
-        
-        print('RUN COMPLETE')
-        run= run + 1
+    for group in groups:
+        run=1
+        for i in range(runs):    
+            print('BEGINNING RUN {}/{}'.format(run, runs))
+            crossval.OuterCV(group)
+            crossval.InnerCV()    
+            features.SelKBest()
+            features.SelKBestOuter()
+            estimators.InnerFolds(group, run)
+            bestest= estpicker.Best(group, run)
+            estimators.OuterFolds(group, run, bestest)
+            bootstrap.Bill(group, run)
+            run= run + 1
+            print('RUN COMPLETE')    
 
     os.system('spd-say -r -50 -p -50 -t female3 "your groups have finished running. To run more groups, you must construct additional pylons."')
     
